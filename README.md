b66d9fe6ff4bef2088ff8561481333e2fe4f1c6e
Kwangjin1986 <kpa35@sfu.ca>
2015-05-07 15:56:15 -0700

    start

---
+++ README.md
@@ -0,0 +1,38 @@
+# Summary analysis of Locomotion experiments on the Multi-worm Tracker
+The aim of this project is to create software that can be used after running
+a 600s basal locomotion experiment using the Multi-worm tracker (Swierczek et al., 2011)
+to generate summary figures and statistics for that experiment. 
+
+This script also backs up experiment .zip files to a webdav server specified by the
+user. 
+
+## Figures it generates
+* Speed versus time over experiment duration
+* Radial and boxplot pathlength figures from 530-590s
+* Spontaneous reversals/minute box plot (averaged over 470-590s)
+* Body size box plots (area, length and width)
+
+## Statistics reported
+* Mean initial speed (averaged over 30-45s) for each strain, and ANCOVA comparing to wild-type
+* Mean final speed (averaged over 575-590s) for each strain, and ANCOVA comparing to wild-type
+* Mean pathlength measured over 530-590s for each strain, and ANCOVA comparing to wild-type
+* Mean number of spontaneous reversals/minute (averaged over 470-590s) for each strain, and ANCOVA comparing to wild-type
+* Mean area, length and width for each strain, and ANCOVA comparing to wild-type
+
+## How to use it
+
+* Set working directory to project's root directory
+
+* Call locomotion_driver.sh from the Bash Shell
+
+* locomotion_driver.sh requires the following arguments from the user: webdav server URL,
+path on webdav where .zip folders should be saved, the path to chore.jar 
+(offline analys program Choreography), and the gigabytes of memory to be used to run 
+Choreography. See example below:
+
+~~~
+bash bin/locomotion_driver.sh https://webdav.server/location folder_to_backup_to 
+/Users/this_user/Chore.jar 16
+~~~
+
+* This code is not working yet. More instructions to come as code is developed further.
---
+++ bin/locomotion_driver.sh
@@ -0,0 +1,52 @@
+## This is the driver script that will call modulars scripts to attack each chunk
+## of the problem
+##
+## Set working directory to project's root directory
+##
+## Requires the following input from the user:
+##		$1: webdav server URL 
+##		$2: path on webdav where .zip folders should be saved (including where it is 
+##			mounted on your computer (e.g /path_on_webdav)
+##		$3: path to chore.jar (offline analys program Choreography)
+##		$4: gigabytes of memory to be used to run Choreography (dependent upon
+##			the machine you are using
+
+## Connect to webdav (so you can backup files)
+## you will be prompted for your webdav username and password
+mount_webdav -i  $1 /
+
+## zip all folders all MWT data folders in directory to be analyzed
+## must be in "data" working directory 
+cd data
+for foldername in *; do cd $foldername; zip ../$foldername *; cd ..; done
+
+## destroy/delete unzipped MWT folder
+for uncompressed in */; do rm -r $uncompressed/; done
+
+## copy .zip files to a webdav server
+## Note - this is very slow...
+cp *.zip $2
+
+## call choreography to analyze the MWT data (each .zip in the folder data)
+## error: Exactly one filename required
+##  Use --help to list valid options.
+for zipfolder in *.zip; do java -Xmx$4g -jar $3 --shadowless -p 0.027 -M 2 -t 20 -S -N all -o fDpesSlLwWaAmMkbPcdxyuvorC1234 --plugin Reoutline::despike --plugin Respine --plugin MeasureReversal::all $zipfolder; done
+
+## move unzipped folder into a new directory (called chore_data)
+mv */ $(mkdir chore_data)
+
+## need to create a large file containing all data files with 
+## data, plate name and strain name in each row
+##grep -r '[0-9]' $(find ./data -name '*.dat') > merged.file
+cd chore_data
+for filename in $(find . -name '*.dat'); do grep -H '[0-9]' $filename >> merged.file; done
+cd ../..
+
+## Use regular expressions in R to parse apart the information in the filepath
+## so we can get data, plate ID and strain as delimited columns
+## call R script with the command line using an argument for the filename we want to parse
+   rscript bin/Column_identification_command.R data/chore_data/merged.file 
+
+## create figures
+
+## create stats
